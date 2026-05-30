d797620df0146b689710f6563f8743fbff5be9f3
mdshap <notunmdshaptarshi@gmail.com>
2026-05-30 09:00:50 +0600

    Server is added in the same file and keys are directly put for copying and running from the repo

---
+++ Client/README.md
@@ -0,0 +1,57 @@
+# 🎓 Books Haven
+### A Book Reviewing Platform
+
+![BooksHaven](https://img.shields.io/badge/Books-Haven-green?style=for-the-badge)
+![MERN](https://img.shields.io/badge/MERN-Stack-success?style=for-the-badge)
+![Firebase](https://img.shields.io/badge/Firebase-Authentication-orange?style=for-the-badge)
+
+---
+
+## 🚀 Live Links
+- 🌐 **Live Website:** [https://zingy-starship-a00546.netlify.app/](https://gleaming-peony-d597c8.netlify.app/#/)
+- 🖥️ **Backend API:** [https://assignment-11-sever.onrender.com/](https://assignment-10-serverside-gyny.onrender.com/)
+
+---
+
+## 📌 Project Overview
+
+**Scholar Stream** is a full-stack scholarship management system that enables students to discover scholarships, apply online, complete secure payments, track application status, and submit reviews.
+
+Admins and moderators can manage scholarships, applications, reviews, and analytics through a secure dashboard.
+
+---
+
+## ✨ Features
+- Users can find books
+- Users can put review of their read books
+
+---
+
+## 🧱 Tech Stack
+
+### 🌐 Frontend
+- React.js  
+- React Router  
+- Tailwind CSS  
+- DaisyUI  
+- Framer Motion  
+- React Hot Toast  
+
+### 🖥️ Backend
+- Node.js  
+- Express.js  
+- MongoDB  
+
+### 🔐 Authentication
+- Firebase Authentication  
+  - Email & Password  
+  - Google Sign-In  
+
+---
+
+## 🗂️ Database Collections
+- Books
+- Reviews  
+
+---
+
---
+++ Server/index.js
@@ -0,0 +1,136 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://booksHaven:6fgST8PpMukfzMzE@cluster0.blss57h.mongodb.net/?appName=Cluster0";
+
+  
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send("User Server Is Available");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected");
+
+    const db = client.db("booksHavenDB");
+    const booksCollection = db.collection("books");
+    const commentsCollection = db.collection("comments");
+
+    app.get("/books", async (req, res) => {
+      const books = booksCollection.find().sort({ createdAt: -1 });
+      const result = await books.toArray();
+      res.send(result);
+    });
+
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booksCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/books/:id/comments", async (req, res) => {
+      const bookId = req.params.id;
+      const cursor = commentsCollection
+        .find({ bookId })
+        .sort({ createdAt: -1 });
+      const comments = await cursor.toArray();
+
+      res.send(comments);
+    });
+
+    app.post("/books/:id/comments", async (req, res) => {
+      const bookId = req.params.id;
+      const { userName, userEmail, text, photoURL } = req.body;
+
+      const comment = {
+        bookId,
+        userName: userName || "Anonymous",
+        userEmail,
+        text,
+        photoURL: photoURL || null,
+        createdAt: new Date(),
+      };
+
+      const result = await commentsCollection.insertOne(comment);
+      res.send(result);
+    });
+
+    app.get("/popular-books", async (req, res) => {
+      const books = booksCollection.find().sort({ createdAt: -1 }).limit(5);
+      const result = await books.toArray();
+      res.send(result);
+    });
+
+    app.get("/top-rated-books", async (req, res) => {
+      const books = booksCollection.find().sort({ rating: -1 }).skip(3).limit(4);
+      const result = await books.toArray();
+      res.send(result);
+    });
+
+    app.post("/books", async (req, res) => {
+      const newBook = {
+        ...req.body,
+        createdAt: new Date(),
+      };
+      const result = await booksCollection.insertOne(newBook);
+      res.send(result);
+    });
+
+    app.get("/my-books/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const books = booksCollection.find(query).sort({ rating: -1 });
+      const result = await books.toArray();
+      res.send(result);
+    });
+
+    app.patch("/my-books/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedData,
+      };
+
+      const result = await booksCollection.updateOne(filter, update);
+      res.send(result);
+    });
+
+    app.delete("/my-books/:id/:email", async (req, res) => {
+      const { id, email } = req.params;
+
+      const query = { _id: new ObjectId(id), userEmail: email };
+      const result = await booksCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("User server is started on port:", port);
+});
